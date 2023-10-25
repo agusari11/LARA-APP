824bb1607739346b36849273bc21ece0d68b1bbb
agusari11 <agus29211@gmail.com>
2023-10-25 09:28:05 +0700

    first conmmit

---
+++ resources/views/about.blade.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>About</title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+
+
+  <style>
+    body {
+      font-family: serif
+    }
+  </style>
+</head>
+<body>
+    <div class="container-fluid">
+    <h1>Full stack Laravel Developer</h1>
+    <p>I'm a professional laravel developer from indonesia i have experience in web developer since 2019 to 2023</p>
+  </div>
+  <div class="card-group">
+    <div class="card" style="background-color: bisque">
+      <img src="hello.png" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">full stack</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Diposting 2 Menit Lalu</small></p>
+      </div>
+    </div>
+    <div class="card" style="background-color: blueviolet">
+      <img src="gambar4.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Front End</h5>
+        <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
+        <p class="card-text"><small class="text-muted">Diposting 2 Menit Lalu</small></p>
+      </div>
+    </div>
+    <div class="card" style="background-color: chartreuse">
+      <img src="gambar4.jpg" class="card-img-top" alt="...">
+      <div class="card-body">
+        <h5 class="card-title">Back End</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
+        <p class="card-text"><small class="text-muted">Diposting 2 Menit Lalu</small></p>
+      </div>
+    </div>
+  </div>
+  <ul class="list-group">
+    <li class="list-group-item">An item</li>
+    <li class="list-group-item">A second item</li>
+    <li class="list-group-item">A third item</li>
+    <li class="list-group-item">A fourth item</li>
+    <li class="list-group-item">And a fifth one</li>
+  </ul>
+  <div class="container">
+      <h1>ini adalah home</h1>
+      <h2>Selamat datang, (( $name ))</h2>
+  </div>
+</body>
+</html>
